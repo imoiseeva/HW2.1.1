c56cd9deb79e02a4bdc7946d9a424f4493ce0716
imoiseeva <moiseeva.irina@gmail.com>
2021-01-27 21:55:41 +0200

    Initial Commit

---
+++ HW2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1.1
+//
+//  Created by Irina Moiseeva on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
